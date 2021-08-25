433eaee244d8d11215713f7429994f8761b784d7
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2021-08-25 09:40:04 -0400

    add train question

---
+++ src/train.ts
@@ -0,0 +1,6 @@
+/** 
+    Given the arrival and departure times of all trains 
+    that reach a railway station, the task is to find 
+    the minimum number of platforms required for the 
+    railway station so that no train waits. 
+*/
