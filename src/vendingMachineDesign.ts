1c761d136440e05ffbdebe2e09de13a507907999
Raghav Narayanan <>
2022-04-06 16:13:51 -0400

    adding vending machine question

---
+++ src/vendingMachineDesign.ts
@@ -0,0 +1,7 @@
+/**
+ * Consider you are designing a Soda Vending Machine.
+ * What features would you include as part of your product? 
+ * What data will you collect to help you achieve your features? How will you store/retrieve/access them?
+ * Pick one use case and talk us through whats going on behind the scene (method/API calls, state changes, DB access, network calls, etc).
+ * Come up with test cases to break the system.
+ */
